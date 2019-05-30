a9b813e2657247b613480ca8f3baf17738e83292
ksetoue <karoline.kimiko@gmail.com>
2019-05-30 00:45:07 -0300

    Set up promises to handles requests to maps API separately

--- location-finder/src/services/PlacesService.js
+++ location-finder/src/services/PlacesService.js
@@ -1,23 +1,52 @@
-import 'dotenv/config';
 import client from '@google/maps';
+import 'dotenv/config';
+
+import Location from '../model/location';
 
 const googleMapsClient = client.createClient({ 
-    key: `${process.env.GOOGLE_PLACES_API_KEY}`,
-    Promise: Promise, 
+    key: `${ process.env.GOOGLE_PLACES_API_KEY }`,
+    Promise: Promise
 });
 
 let PlacesServices = {};
 
-PlacesServices.searchPlaces = (...places) => {
+function getPlacesMapsAPI (queryItem) {
+    return new Promise((resolve, reject) => {
+        googleMapsClient.places({ query: `${queryItem}` }) 
+            .asPromise()
+            .then((response) => {   
+                let locationObj = response.json.results[0]; 
+        
+                let location = new Location( 
+                    locationObj.id,
+                    locationObj.name, 
+                    locationObj.geometry.location.lat,
+                    locationObj.geometry.location.lng,
+                    locationObj.formatted_address
+                )
+                resolve(location);
+            })
+            .catch((err) => {
+                reject(console.log(err));
+            });
+    });
+}
 
-    googleMapsClient.places({ query: 'statue of liberty' })
-        .asPromise() 
-        .then((response) => {
-            console.log('response: ', response);    
-            console.log(response.json.results);
-        })
-        .catch((err) => {
-            console.log(err);
+PlacesServices.searchPlaces = (places) => {
+    if (!places) {
+        return new Error('This is a invalid input. Please, see the documentation for more info.\n');
+    }
+
+    let locations = []; 
+    
+    places.forEach( element => {
+        let promise = getPlacesMapsAPI(element);
+        locations.push(promise);
+    });
+
+    Promise.all(locations)
+        .then(results => {
+            console.log(results);
         });
 }
 

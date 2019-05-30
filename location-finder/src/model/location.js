e5e6b60f599fb50ba38aa8b7e078a8f8055356f2
ksetoue <karoline.kimiko@gmail.com>
2019-05-30 00:43:17 -0300

    Add model for location object

---
+++ location-finder/src/model/location.js
@@ -0,0 +1,10 @@
+
+export default class Location {
+    constructor(id, name, lat, long, address) {
+        this._id = id;
+        this._name = name;
+        this._lat = lat; 
+        this._long = long; 
+        this._addess = address;
+    }
+}

44cb35117f4c5ef6e90ade62bf98956679e7423c
ksetoue <karoline.kimiko@gmail.com>
2019-05-30 12:08:50 -0300

    Add .map to PlaceServices

--- location-finder/src/controllers/PlacesController.js
+++ location-finder/src/controllers/PlacesController.js
@@ -1,14 +1,28 @@
 
 import PlaceServices from '../services/PlacesService'; 
-import DistanceServices from '../services/DistanceService'; 
+// import DistanceServices from '../services/DistanceService'; 
 
 let PlacesController = {}; 
 
 PlacesController.getLocations = (req, res) => {
-    let locations = req.body; 
-    console.log(locations);
+    let places = req.body; 
 
-    PlaceServices.searchPlaces(locations);
+    if (!places) {
+        return new Error('This is a invalid input. Please, see the documentation for more info.\n');
+    }
+
+    let locations = places.map(el => {
+        return PlaceServices.searchPlaces(el);
+    });
+    
+    Promise.all(locations)
+        .then(results => {
+            return results;
+        })
+        .catch(err => {
+            return new Error({ message: `An error ocurred while processing location coordinates: ${err}` });
+        });
+
 
     return res.status(200).json({ message: 'Hello from test controller' });
 }

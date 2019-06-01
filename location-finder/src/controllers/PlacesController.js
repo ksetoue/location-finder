de75de3b5ffc0c17ffcf588feb777e4e8165054e
ksetoue <karoline.kimiko@gmail.com>
2019-05-31 22:42:45 -0300

    Change Places controller to return a JSON with pairs of locations

--- location-finder/src/controllers/PlacesController.js
+++ location-finder/src/controllers/PlacesController.js
@@ -1,10 +1,41 @@
 
-import fs from 'fs';
 import getMock from '../services/GetMock';
 import PlaceServices from '../services/PlacesService'; 
 import DistanceServices from '../services/DistanceService'; 
 
 let PlacesController = {}; 
+
+function mountPair (location) {
+    if (!location.near) {
+        return null; 
+    }
+
+    let obj = {};
+    obj.id = location.id; 
+    obj.name = location.name; 
+    obj.lat = location.lat; 
+    obj.long = location.long; 
+
+    return obj;
+}
+
+function getPairs (processedLocations) {
+    let pairs = []; 
+
+    processedLocations.forEach(element => {
+        let l1 = mountPair(element);
+        let l2 = mountPair(element.near); 
+
+        let pair = {
+            location1: l1,
+            location2: l2
+        }
+        
+        pairs.push(pair);
+    });
+
+    return pairs;
+}
 
 PlacesController.getLocations = (req, res) => {
     let places = req.body; 
@@ -26,18 +57,33 @@ PlacesController.getLocations = (req, res) => {
                 processableLocations.push(result);
             }
 
-            res.status(200).json(DistanceServices.getDistances(processableLocations));
+            let processedLocations = DistanceServices.getDistances(processableLocations);
+
+            res.status(200).json(getPairs(processedLocations));
         })
         .catch(err => res.status(500).json(err));
 
-    // work in progress
+    // work in progress - without mock
     // let locations = places.map(el => {
     //     return PlaceServices.searchPlaces(el);
     // });
     
     // Promise.all(locations)
     //     .then( results => {
-    //         //add logic
+    //         let processableLocations = [];
+    //         let unprocessable = []; 
+
+    //         for(let result of results) {
+    //             if(!result.id) {
+    //                 unprocessable.push(result);
+    //                 continue;
+    //             }
+    //             processableLocations.push(result);
+    //         }
+
+    //         let processedLocations = DistanceServices.getDistances(processableLocations);
+
+    //         res.status(200).json(getPairs(processedLocations));
     //     })
     //     .catch(err => res.status(500).json(err));
 
